1511a3ef1848c5b4b4ee2b468f833af6ea97d140
( ͡° ͜ʖ ͡°) <josselin.gayot@campus.academy>
2021-02-01 12:11:20 +0100

    Update Austin.php

--- Austin.php
+++ Austin.php
@@ -1,23 +1,28 @@
 <?php
-class Hero {
-    public function __construct() {
-        echo "<pre>Creation of a Hero called $name.</pre> ";
+class Austin {
+    public function __construct($name) {
+        echo "<pre>Cachez vous Mesdames, voici $name.</pre> ";
         $this->name = "Austin";
         $this->life = 100;
         $this->damage = 10;
+        $this->mojo = 1000;
     }
+
     public function talk($message = 'Salut Chérie') {
         echo ($this->name).":".$message."</pre><br />";
     }
+
     public function attack($monster) {
-        $this->talk("Prend ça, suppot de Dr Denfer ! $monster->name");
+        $this->talk("Prend ça, suppot du Docteur Denfer ! $monster->name");
         $monster->isHit($this);
     }
+
     public function __toString() {
         return "Hero - $this->name [life=$this->life , damage=$this->damage]";
     }
-    public function isHit($monster) {
-        $this->talk("Aïe, j'ai mal !");
+
+    public function isHit($Austin) {
+        $this->talk("Aïe, j'ai être matinal, j'ai mal !");
         $this->life = $this->life - $monster->damage;
     }
 }
